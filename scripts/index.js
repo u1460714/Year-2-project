4c66762d4f9e5a84d5cda5bb856f37c5f82a2b35
u1460714 <tyler_flynn@outlook.com>
2017-02-12 21:15:57 +0000

    UAapp.v3
    Changes to the homepage theme such as colours, Login page is started with HTML,CSS but still has JS to do.

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+﻿function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+/*
+function checkDetails(){
+
+    var usernameConfirmation=document.getElementById("username");
+    var username=usernameConfirmation.value
+    if(username.length <5)
+        alert("too_short");
+}
+var btn = document.getElementById("loginButton");
+btn.addEventListener("click", checkDetails, false);
+
+
+   
+    
+
+function confirmEmail(){
+    var emailAddressConfirm=document.getElementById("Email");
+    var emailAddress=emailAddressConfirm.value
+    if(emailAddress==="tyler_flynn@live.co.uk")
+    {
+        console.log("Correct!");
+    }
+    else
+    {
+        console.log("Nope!");
+    }
+}; 
+*/
